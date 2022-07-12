6426da72661ceddf9766c96830837f5059dc65a4
daryllmamaril <dazzmamaril@gmail.com>
2022-07-12 23:03:49 +0800

    Ball Component displaying a ball with number

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Ball from "./Ball";
+
+function App() {
+  return (
+    <div className="App">
+      <Ball num={17} />
+      <Ball num={17} />
+      <Ball num={17} />
+      <Ball num={17} />
+    </div>
+  );
+}
+
+export default App;
